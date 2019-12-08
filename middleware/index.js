8d7bd7b40fbd1ca15c62bc1fc72b1028fdece13b
amberstyers <amberstyers@gmail.com>
2019-12-08 13:13:49 -0600

    almost done

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,35 @@
+
+  
 const jwt = require('jsonwebtoken')
 
-const logger = () => {}
-
-const authenticate = () => {}
-
-module.exports = {
-  logger,
-  authenticate
+const logger = (req, res, next) => {
+  console.log('Logging route:' ,'/users', new Date().toISOString())
+  next()
 }
+
+const authenticate = (req, res, next) => {
+  let header = req.headers['authorization']
+  console.log(header)
+  
+  header = header.split(' ')
+  console.log(header)
+
+  let token = header[1]
+
+  console.log(token)
+
+  jwt.verify(token, 'secret', function(err, decoded){
+    if(err){
+      console.log('Invalid Token')
+    }else{
+      req.user = decoded
+      console.log('success')
+    }
+    console.log(decoded)
+  })
+  next()
+
+  module.exports = {
+    logger,
+    authenticate
+  }
