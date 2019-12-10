d8b597f2d0f9682cc60b713c14f0c10b37c979da
amberstyers <amberstyers@gmail.com>
2019-12-09 18:16:01 -0600

    something more

--- middleware/index.js
+++ middleware/index.js
@@ -1,35 +1,46 @@
 
   
+// const jwt = require('jsonwebtoken')
+
+// const logger = (req, res, next) => {
+//   console.log('Logging route:' ,'/users', new Date().toISOString())
+//   next()
+// }
+
+// const authenticate = (req, res, next) => {
+//   let header = req.headers['authorization']
+//   console.log(header)
+  
+//   header = header.split(' ')
+//   console.log(header)
+
+//   let token = header[1]
+
+//   console.log(token)
+
+//   jwt.verify(token, 'secret', function(err, decoded){
+//     if(err){
+//       console.log('Bad Token')
+//     }else{
+//       req.user = decoded
+//       console.log('Triumph')
+//     }
+//     console.log(decoded)
+//   })
+//   next()
+
+//   module.exports = {
+//     logger,
+//     authenticate
+//   }
+
 const jwt = require('jsonwebtoken')
 
-const logger = (req, res, next) => {
-  console.log('Logging route:' ,'/users', new Date().toISOString())
-  next()
+const logger = () => {}
+
+const authenticate = () => {}
+
+module.exports = {
+  logger,
+  authenticate
 }
-
-const authenticate = (req, res, next) => {
-  let header = req.headers['authorization']
-  console.log(header)
-  
-  header = header.split(' ')
-  console.log(header)
-
-  // let token = header[1]
-
-  console.log(token)
-
-  jwt.verify(token, 'secret', function(err, decoded){
-    if(err){
-      console.log('Bad Token')
-    }else{
-      req.user = decoded
-      console.log('Triumph')
-    }
-    console.log(decoded)
-  })
-  next()
-
-  module.exports = {
-    logger,
-    authenticate
-  }
